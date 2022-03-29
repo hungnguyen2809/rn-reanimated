0824433911fdc41cec330f95b04b582d80c71bb5
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2022-03-29 15:16:57 +0700

    Double Tab Like RN

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import PanResponderRN from './src/RNPanResponder';
+import DoubleTabLikeRN from './src/DoubleTapLikeRN';
 
 function App() {
   return (
     <View style={styles.main}>
       {/* <AnimatedRNTutorial /> */}
-      <PanResponderRN />
+      {/* <PanResponderRN /> */}
+      <DoubleTabLikeRN />
       {/* <Introduction /> */}
       {/* <PanGestureHandlerExam /> */}
       {/* <ScrollViewPage /> */}
---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+const icons = {
+  heart: require('./heart.png'),
+  heartOutline: require('./heart-outline.png'),
+};
+
+export default icons;
